f891c655610483b0d44b4d6ca72c7b2c10408303
mcagabe19 <82843871+mcagabe19@users.noreply.github.com>
2022-12-18 15:14:22 +0300

    Update Replay.hx

--- source/Replay.hx
+++ source/Replay.hx
@@ -68,17 +68,17 @@ class Replay
         var data:String = Json.stringify(json);
 
         #if sys
-        File.saveContent("assets/replays/replay-" + PlayState.SONG.song + "-time" + Date.now().getTime() + ".kadeReplay", data);
+        File.saveContent(SUtil.getStorageDirectory() + "assets/replays/replay-" + PlayState.SONG.song + "-time" + Date.now().getTime() + ".kadeReplay", data);
         #end
     }
 
     public function LoadFromJSON()
     {
         #if sys
-        trace('loading ' + Sys.getCwd() + 'assets/replays/' + path + ' replay...');
+        trace('loading ' + #if mobile SUtil.getStorageDirectory() + #else Sys.getCwd() + #end 'assets/replays/' + path + ' replay...');
         try
         {
-            var repl:ReplayJSON = cast Json.parse(File.getContent(Sys.getCwd() + "assets/replays/" + path));
+            var repl:ReplayJSON = cast Json.parse(File.getContent(#if mobile SUtil.getStorageDirectory() + #else Sys.getCwd() + #end "assets/replays/" + path));
             replay = repl;
         }
         catch(e)
