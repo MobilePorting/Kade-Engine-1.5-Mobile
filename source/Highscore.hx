6e6bc8dbb22150e21db225861744c10f39c40271
mcagabe19 <82843871+mcagabe19@users.noreply.github.com>
2022-12-19 11:30:30 +0300

    Update Highscore.hx

--- source/Highscore.hx
+++ source/Highscore.hx
@@ -29,11 +29,6 @@ class Highscore
 
 	public static function saveWeekScore(week:Int = 1, score:Int = 0, ?diff:Int = 0):Void
 	{
-
-		#if !switch
-		NGio.postScore(score, "Week " + week);
-		#end
-
 		if(!FlxG.save.data.botplay)
 		{
 			var daWeek:String = formatSong('week' + week, diff);
