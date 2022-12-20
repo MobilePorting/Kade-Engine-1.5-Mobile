80f6f81b8fd899c266003005e56c034afce06238
mcagabe19 <82843871+mcagabe19@users.noreply.github.com>
2022-12-20 22:17:56 +0300

    Update Controls.hx

--- source/Controls.hx
+++ source/Controls.hx
@@ -336,7 +336,7 @@ class Controls extends FlxActionSet
 			case B:
 				inline forEachBound(Control.BACK, (action, state) -> addButtonUI(action, VirtualPad.buttonB, state));
                         case P:
-				inline forEachBound(Control.PAUSE, (action, state) -> addbuttonUI(action, VirtualPad.buttonP, state));
+				inline forEachBound(Control.PAUSE, (action, state) -> addButtonUI(action, VirtualPad.buttonP, state));
 			case A_B | A_B_C | A_B_E | A_B_X_Y | A_B_C_X_Y | A_B_C_X_Y_Z | A_B_C_D_V_X_Y_Z:
 				inline forEachBound(Control.ACCEPT, (action, state) -> addButtonUI(action, VirtualPad.buttonA, state));
 				inline forEachBound(Control.BACK, (action, state) -> addButtonUI(action, VirtualPad.buttonB, state));
@@ -382,7 +382,7 @@ class Controls extends FlxActionSet
 			case B:
 				inline forEachBound(Control.BACK, (action, state) -> addButtonNOTES(action, VirtualPad.buttonB, state));
                         case P:
-				inline forEachBound(Control.PAUSE, (action, state) -> addbuttonNOTES(action, VirtualPad.buttonP, state));
+				inline forEachBound(Control.PAUSE, (action, state) -> addButtonNOTES(action, VirtualPad.buttonP, state));
 			case A_B | A_B_C | A_B_E | A_B_X_Y | A_B_C_X_Y | A_B_C_X_Y_Z | A_B_C_D_V_X_Y_Z:
 				inline forEachBound(Control.ACCEPT, (action, state) -> addButtonNOTES(action, VirtualPad.buttonA, state));
 				inline forEachBound(Control.BACK, (action, state) -> addButtonNOTES(action, VirtualPad.buttonB, state));
