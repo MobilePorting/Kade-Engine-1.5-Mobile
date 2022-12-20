094063f2e2aa046b00294d4052a63c64315c1f26
mcagabe19 <82843871+mcagabe19@users.noreply.github.com>
2022-12-20 10:52:46 +0300

    Update PlayState.hx

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1600,6 +1600,13 @@ class PlayState extends MusicBeatState
 			babyArrow.animation.play('static');
 			babyArrow.x += 50;
 			babyArrow.x += ((FlxG.width / 2) * player);
+
+                        if (FlxG.save.data.middleScroll) {
+				babyArrow.x -= 275;
+				if (player != 1) {
+					babyArrow.visible = false;
+				}
+			}
 			
 			cpuStrums.forEach(function(spr:FlxSprite)
 			{					
