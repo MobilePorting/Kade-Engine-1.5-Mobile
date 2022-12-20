4a48f683b6fe2eea594b0198983c1bfcc5e884fe
mcagabe19 <82843871+mcagabe19@users.noreply.github.com>
2022-12-20 10:51:10 +0300

    Update OptionsMenu.hx

--- source/OptionsMenu.hx
+++ source/OptionsMenu.hx
@@ -27,6 +27,7 @@ class OptionsMenu extends MusicBeatState
 		new OptionCategory("Gameplay", [
 			//new DFJKOption(controls),
 			new DownscrollOption("Change the layout of the strumline."),
+                        new MiddleScroll("Toggle The Middlescroll"),
 			new GhostTapOption("Ghost Tapping is when you tap a direction and it doesn't give you a miss."),
 			new Judgement("Customize your Hit Timings (LEFT or RIGHT)"),
 			new FPSCapOption("Cap your FPS"),
