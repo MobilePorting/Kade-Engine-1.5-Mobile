ce12aea0b892f87ac69239f6858b7f89721b95d4
mcagabe19 <82843871+mcagabe19@users.noreply.github.com>
2022-12-23 22:43:08 +0300

    bruh moment kade.

--- source/KeyBindMenu.hx
+++ source/KeyBindMenu.hx
@@ -248,11 +248,11 @@ class KeyBindMenu extends FlxSubState
 
         if(shouldReturn){
             keys[curSelected] = r;
-            FlxG.sound.play(Paths.sound('Hover','clown'));
+            //FlxG.sound.play(Paths.sound('Hover','clown'));
         }
         else{
             keys[curSelected] = tempKey;
-            FlxG.sound.play(Paths.sound('confirm','clown'));
+            //FlxG.sound.play(Paths.sound('confirm','clown'));
             keyWarning.alpha = 1;
             warningTween.cancel();
             warningTween = FlxTween.tween(keyWarning, {alpha: 0}, 0.5, {ease: FlxEase.circOut, startDelay: 2});
