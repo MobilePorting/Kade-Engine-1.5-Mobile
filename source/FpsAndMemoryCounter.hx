44b0b1d6ff55b1da932dc2abf006f3d4f0d418c3
mcagabe19 <82843871+mcagabe19@users.noreply.github.com>
2022-12-23 12:52:38 +0300

    Update FpsAndMemoryCounter.hx

--- source/FpsAndMemoryCounter.hx
+++ source/FpsAndMemoryCounter.hx
@@ -76,11 +76,6 @@ class FpsAndMemoryCounter extends TextField
 		var currentCount = times.length;
 		currentFPS = Math.round((currentCount + cacheCount) / 2);
 
-		if (currentFPS > Main.framerate)
-		{
-			currentFPS = Main.framerate;
-		}
-
 		if (currentCount != cacheCount /*&& visible*/)
 		{
                 text = "FPS: " + currentFPS;
