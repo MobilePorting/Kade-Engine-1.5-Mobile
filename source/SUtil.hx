b220886655adf773ac399d3f489f56a6b959d595
mcagabe19 <82843871+mcagabe19@users.noreply.github.com>
2023-01-17 16:01:56 +0300

    fix

--- source/SUtil.hx
+++ source/SUtil.hx
@@ -101,7 +101,9 @@ class SUtil
 
 		#if mobile
 		if (!sys.FileSystem.exists(SUtil.getStorageDirectory()))
-			Lib.application.window.alert('Please create folder to ' + SUtil.getStorageDirectory() + '\nPress Ok to close the app', 'Error!');
+			Lib.application.window.alert('Please create folder to ' + SUtil.getStorageDirectory(),
+                                + '\nPress Ok to close the app',
+                                'Error!');
 				LimeSystem.exit(1);
 		#end
 	}
