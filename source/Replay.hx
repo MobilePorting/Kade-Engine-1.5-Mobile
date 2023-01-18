85c80dbacd4166730e8de152d1687c489d4c435b
mcagabe19 <82843871+mcagabe19@users.noreply.github.com>
2023-01-18 13:28:18 +0300

    better

--- source/Replay.hx
+++ source/Replay.hx
@@ -94,7 +94,7 @@ class Replay
 			+ ' replay...');
 		try
 		{
-			var repl:ReplayJSON = cast Json.parse(File.getContent(#if mobile SUtil.getStorageDirectory() + #else Sys.getCwd() + #end#if mobile "replays/" + #else "assets/replays/" + #end path));
+			var repl:ReplayJSON = cast Json.parse(File.getContent(#if mobile SUtil.getStorageDirectory() + "replays/" + #else Sys.getCwd() + "assets/replays/" + #end path));
 			replay = repl;
 		}
 		catch (e)
